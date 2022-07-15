ac4a989de1b3609b9899106281914032a109a443
MarcinMaster <m.luba505@gmail.com>
2022-07-15 06:31:18 +0200

    Added a MUI Checkout Comp

---
+++ src/components/checkout/Styles.js
@@ -0,0 +1,10 @@
+import { makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles({
+    stepButtons: {
+        display: 'flex',
+        flexDirection: 'flex-end'
+    }
+});
+
+export default useStyles;
